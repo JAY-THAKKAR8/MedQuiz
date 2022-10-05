ecde47a6216830b0101f3fbb092256cdd743ce06
JAY-THAKKAR8 <jaythakkar2724@gmail.com>
2022-10-05 18:43:16 +0530

    add common widgets

---
+++ medQuiz/lib/utils/theme/medQuiz_style.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:medquiz/utils/theme/medQuiz_colors.dart';
+
+class AppTextStyles {
+
+  static const appbarTextStyle = TextStyle(
+      fontSize: 20,
+      color: MedQuizColors.whiteColor,
+      );
+
+  static const normalTextTextStyle = TextStyle(
+      fontSize: 18,
+      color: MedQuizColors.blackColor,
+      fontWeight: FontWeight.w400
+  );
+
+  static const subtextTextStyle = TextStyle(
+      fontSize: 16,
+      color: MedQuizColors.blackColor,
+  );
+
+  static const headingTextStyle = TextStyle(
+      fontSize: 20,
+      color: MedQuizColors.blackColor,
+      fontWeight: FontWeight.w500
+  );
+
+
+
+}
