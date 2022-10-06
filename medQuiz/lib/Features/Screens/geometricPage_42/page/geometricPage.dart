c467088d213fa6a3d9eeaa1a145a04a6ed4a1451
badgujarpoonam <badgujarpoonam2097@gmail.com>
2022-10-06 12:50:41 +0530

    new page_42

---
+++ medQuiz/lib/Features/Screens/geometricPage_42/page/geometricPage.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:medquiz/utils/theme/medQuiz_colors.dart';
+import 'package:medquiz/utils/theme/medQuiz_style.dart';
+
+class GeometricPageUi extends StatefulWidget {
+  const GeometricPageUi({Key? key}) : super(key: key);
+
+  @override
+  State<GeometricPageUi> createState() => _GeometricPageUiState();
+}
+
+class _GeometricPageUiState extends State<GeometricPageUi> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: MedQuizColors.darkBlueColor,
+         leading: Icon(Icons.arrow_back_ios,color: MedQuizColors.whiteColor),
+        title: Text("Geometric weekly test",style: AppTextStyles.appbarTextStyle),
+        bottom: PreferredSize(
+          preferredSize: Size.fromHeight(200),
+          child: Container(
+            color: MedQuizColors.darkBlueColor,
+            child: Row(
+              children: [
+                   Column(
+                     children: [
+                       CircleAvatar(
+
+                       )
+                     ],
+                   )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
