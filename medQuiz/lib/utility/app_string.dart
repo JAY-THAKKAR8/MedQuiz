e959fb217c1d06a6a4f30c9df8c1d72929686476
Rana bhavesh <rbhavesh131@gmail.com>
2022-11-02 17:32:33 +0530

    api manager ,models ,response added

---
+++ medQuiz/lib/utility/app_string.dart
@@ -0,0 +1,8 @@
+class AppString {
+  static const String userToken = 'USER_TOKEN';
+  static const String baseUrl =
+      'https://138.201.86.170/italy-up-api/public/api';
+
+  static const String setting = '$baseUrl/settings';
+  static const String plansUrl = '$baseUrl/plan';
+}
